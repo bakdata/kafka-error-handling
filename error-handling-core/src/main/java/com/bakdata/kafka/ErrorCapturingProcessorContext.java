415961ae927daa09a70808d9c92fd7124d95766b
Markus Kobold <markus.kobold@bakdata.com>
2022-07-08 15:51:07 +0200

    update dependencies, rm incompatible deprecations

--- error-handling-core/src/main/java/com/bakdata/kafka/ErrorCapturingProcessorContext.java
+++ error-handling-core/src/main/java/com/bakdata/kafka/ErrorCapturingProcessorContext.java
@@ -48,18 +48,4 @@ final class ErrorCapturingProcessorContext extends DecoratorProcessorContext {
         final ProcessedKeyValue<Object, Object, V> recordWithOldKey = getValue(value);
         super.forward(key, recordWithOldKey);
     }
-
-    @Deprecated
-    @Override
-    public <K, V> void forward(final K key, final V value, final int childIndex) {
-        final ProcessedKeyValue<Object, Object, V> recordWithOldKey = getValue(value);
-        super.forward(key, recordWithOldKey, childIndex);
-    }
-
-    @Deprecated
-    @Override
-    public <K, V> void forward(final K key, final V value, final String childName) {
-        final ProcessedKeyValue<Object, Object, V> recordWithOldKey = getValue(value);
-        super.forward(key, recordWithOldKey, childName);
-    }
 }
