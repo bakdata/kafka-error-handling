434cd5a6496e152564f7c76b04863ce5b15fe467
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-04-24 12:58:35 +0200

    Migrate to version catalog (#33)

--- error-handling-avro/build.gradle.kts
+++ error-handling-avro/build.gradle.kts
@@ -2,7 +2,7 @@ description = "Transform dead letters in Kafka Streams applications to Avro form
 
 plugins {
     id("java-library")
-    id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
+    alias(libs.plugins.avro)
 }
 
 // add .avsc files to jar allowing us to use them in other projects as a schema dependency
@@ -15,26 +15,16 @@ sourceSets {
 }
 
 dependencies {
-    val avroVersion: String by project
-    api(group = "org.apache.avro", name = "avro", version = avroVersion)
+    api(libs.avro)
     api(project(":error-handling-core"))
 
-    val junitVersion: String by project
-    testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
+    testRuntimeOnly(libs.junit.platform.launcher)
+    testImplementation(libs.junit.jupiter)
     testImplementation(testFixtures(project(":error-handling-core")))
-    val mockitoVersion: String by project
-    testImplementation(group = "org.mockito", name = "mockito-junit-jupiter", version = mockitoVersion)
-    val assertJVersion: String by project
-    testImplementation(group = "org.assertj", name = "assertj-core", version = assertJVersion)
-    val log4jVersion: String by project
-    testImplementation(group = "org.apache.logging.log4j", name = "log4j-slf4j2-impl", version = log4jVersion)
-    val kafkaStreamsTestsVersion: String by project
-    testImplementation(
-        group = "com.bakdata.fluent-kafka-streams-tests",
-        name = "fluent-kafka-streams-tests-junit5",
-        version = kafkaStreamsTestsVersion
-    )
-    testImplementation(group = "io.confluent", name = "kafka-streams-avro-serde")
+    testImplementation(libs.mockito.junit)
+    testImplementation(libs.assertj)
+    testImplementation(libs.log4j.slf4j2)
+    testImplementation(libs.kafka.streams.avro.serde)
 }
 
 avro {
