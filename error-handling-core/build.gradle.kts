27ec8455172f8fc71378b56c79ee619d2541ba35
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-04-24 12:33:58 +0200

    Migrate to version catalog

--- error-handling-core/build.gradle.kts
+++ error-handling-core/build.gradle.kts
@@ -2,51 +2,26 @@ description = "A library for error handling in Kafka Streams."
 
 plugins {
     id("java-library")
-    id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
+    alias(libs.plugins.avro)
 }
 
 dependencies {
-    api(platform("com.bakdata.kafka:kafka-bom:1.1.0"))
-    api(group = "org.apache.kafka", name = "kafka-streams")
-    val avroVersion: String by project
-    implementation(group = "org.apache.avro", name = "avro", version = avroVersion)
-    implementation(group = "org.jooq", name = "jool", version = "0.9.15")
-    implementation(group = "org.apache.commons", name = "commons-lang3", version = "3.17.0")
+    api(platform(libs.kafka.bom))
+    api(libs.kafka.streams)
+    implementation(libs.jool)
+    implementation(libs.commons.lang)
 
-    val junitVersion: String by project
-    testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
-    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
-    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-params", version = junitVersion)
-    val assertJVersion: String by project
-    testImplementation(group = "org.assertj", name = "assertj-core", version = assertJVersion)
-    val mockitoVersion: String by project
-    testImplementation(group = "org.mockito", name = "mockito-core", version = mockitoVersion)
-    testImplementation(group = "org.mockito", name = "mockito-junit-jupiter", version = mockitoVersion)
-    val log4jVersion: String by project
-    testImplementation(group = "org.apache.logging.log4j", name = "log4j-slf4j2-impl", version = log4jVersion)
-    val kafkaStreamsTestsVersion: String by project
-    testImplementation(
-        group = "com.bakdata.fluent-kafka-streams-tests",
-        name = "fluent-kafka-streams-tests-junit5",
-        version = kafkaStreamsTestsVersion
-    )
-    testFixturesImplementation(
-        testImplementation(
-            group = "org.junit.jupiter",
-            name = "junit-jupiter-api",
-            version = junitVersion
-        )
-    )
-    testFixturesImplementation(
-        group = "com.bakdata.fluent-kafka-streams-tests",
-        name = "fluent-kafka-streams-tests-junit5",
-        version = kafkaStreamsTestsVersion
-    )
-    testFixturesImplementation(group = "org.apache.logging.log4j", name = "log4j-slf4j2-impl", version = log4jVersion)
-    val jacksonVersion: String by project
-    testFixturesImplementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version = jacksonVersion)
-    testFixturesImplementation(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = jacksonVersion)
-    testFixturesImplementation(group = "com.fasterxml.jackson.datatype", name = "jackson-datatype-jsr310", version = jacksonVersion)
+    testRuntimeOnly(libs.junit.platform.launcher)
+    testFixturesImplementation(libs.junit.jupiter)
+    testImplementation(libs.assertj)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.junit)
+    testImplementation(libs.log4j.slf4j2)
+    testFixturesApi(libs.fluentKafkaStreamsTests)
+    testFixturesImplementation(libs.jackson.core)
+    testFixturesImplementation(libs.jackson.databind)
+    testFixturesImplementation(libs.jackson.datatype.jsr310)
+    testImplementation(libs.avro)
 }
 
 avro {
--- error-handling-proto/build.gradle.kts
+++ error-handling-proto/build.gradle.kts
@@ -2,29 +2,20 @@ description = "Transform dead letters in Kafka Streams applications to protobuf.
 
 plugins {
     id("java-library")
-    id("com.google.protobuf") version "0.9.4"
+    alias(libs.plugins.protobuf)
 }
 
-val protobufVersion: String by project
+val protobufVersion = libs.protobuf.get().version
 dependencies {
     api(project(":error-handling-core"))
-    api(group = "com.google.protobuf", name = "protobuf-java", version = protobufVersion)
-    val junitVersion: String by project
-    testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
+    api(libs.protobuf)
+    testRuntimeOnly(libs.junit.platform.launcher)
+    testImplementation(libs.junit.jupiter)
     testImplementation(testFixtures(project(":error-handling-core")))
-    val mockitoVersion: String by project
-    testImplementation(group = "org.mockito", name = "mockito-junit-jupiter", version = mockitoVersion)
-    val assertJVersion: String by project
-    testImplementation(group = "org.assertj", name = "assertj-core", version = assertJVersion)
-    val log4jVersion: String by project
-    testImplementation(group = "org.apache.logging.log4j", name = "log4j-slf4j2-impl", version = log4jVersion)
-    val kafkaStreamsTestsVersion: String by project
-    testImplementation(
-        group = "com.bakdata.fluent-kafka-streams-tests",
-        name = "fluent-kafka-streams-tests-junit5",
-        version = kafkaStreamsTestsVersion
-    )
-    testImplementation(group = "io.confluent", name = "kafka-streams-protobuf-serde")
+    testImplementation(libs.mockito.junit)
+    testImplementation(libs.assertj)
+    testImplementation(libs.log4j.slf4j2)
+    testImplementation(libs.kafka.streams.protobuf.serde)
 }
 
 protobuf {
